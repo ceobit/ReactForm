3c253bca9230a23fa93d531b207181b8130af929
Roman Lebedenko <ceobit90@gmail.com>
2020-11-14 14:09:30 +0100

    added contract form and WorkStatement form
    front+backend

---
+++ front/src/components/PrintContract/PrintContract.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PrintContractContent from "./PrintContractContent";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  flex: {
+    display: "flex",
+    flexDirection: "column",
+    // justifyContent: "flex-end",
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "center",
+    "@media print": {
+      display: "none",
+    },
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+export default function PrintContract() {
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  const handleBack = () => {
+    history.push("/create");
+  };
+
+  return (
+    <div className={classes.flex}>
+      <PrintContractContent />
+      <div className={classes.buttons}>
+        <style>{`@media print {.buttons{display: none;}}`}</style>
+        <Button
+          onClick={handleBack}
+          className={classes.button}
+          variant="contained"
+          color="primary"
+        >
+          Назад
+        </Button>
+      </div>
+    </div>
+  );
+}
